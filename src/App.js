ac469120f9e45e7cf8d429a605fc0163f64219e0
Erick-Gitbash <licerickps@gmail.com>
2022-04-01 21:35:14 -0600

    Card Erick

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import CardErick from "./components/CardErick";
 import CardGeneral from "./components/CardGeneral";
 
 function App() {
@@ -50,7 +51,7 @@ function App() {
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
             <CardGeneral>
-              {/* llamar componente de  card Erick */}
+              <CardErick name="Erick" image="/erick.jpg" location="Paraíso, Tabasco, MX"/>
             </CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
