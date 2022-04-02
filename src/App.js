152385ed3c83359d404cef4e3ac7daba4f13fe10
HugoRo <ndhugohro@gmail.com>
2022-04-01 20:38:15 -0600

    adding hugo card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import CardGeneral from "./components/CardGeneral";
+import CardHugo from "./components/Card_hugo";
 
 function App() {
   return (
     <>
       <section className="container">
-        <h2 className="text-center my-5">Koders JavaScript 16va Generacción 🤓 🔥</h2>
+        <h2 className="text-center my-5">
+          Koders JavaScript 16va Generacción 🤓 🔥
+        </h2>
       </section>
       <section className="container">
         <div className="row">
@@ -14,57 +17,42 @@ function App() {
             </CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Paco */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Paco */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
             <CardGeneral>
               {/* llamar componente de  card Hugo */}
+              <CardHugo name="Hugo Ro." ubicacion="Zapopan, Jalisco" imagen="hugo.jpeg">
+
+              </CardHugo>
             </CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Hector */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Hector */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Cris*/}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Cris*/}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Jess */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Jess */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Rubs */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Rubs */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Robert */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Robert */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Erick */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Erick */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Heidel */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Heidel */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card fer */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card fer */}</CardGeneral>
           </div>
         </div>
-    </section>
+      </section>
     </>
   );
 }
