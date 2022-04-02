349a71c7034da7cc65819666d7d2db565b2a21ec
Jess Vargas <jesvadtz@gmail.com>
2022-04-01 21:15:24 -0500

    Add Card Jessica Vargas with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import CardGeneral from "./components/CardGeneral";
+import CardJess from "./components/CardJess";
 
 function App() {
   return (
     <>
       <section className="container">
-        <h2 className="text-center my-5">Koders JavaScript 16va Generacción 🤓 🔥</h2>
+        <h2 className="text-center my-5">
+          Koders JavaScript 16va Generacción 🤓 🔥
+        </h2>
       </section>
       <section className="container">
         <div className="row">
@@ -14,57 +17,45 @@ function App() {
             </CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Paco */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Paco */}</CardGeneral>
+          </div>
+          <div class="col-12 col-md-4 col-lg-3 mt-2">
+            <CardGeneral>{/* llamar componente de  card Hugo */}</CardGeneral>
+          </div>
+          <div class="col-12 col-md-4 col-lg-3 mt-2">
+            <CardGeneral>{/* llamar componente de  card Hector */}</CardGeneral>
+          </div>
+          <div class="col-12 col-md-4 col-lg-3 mt-2">
+            <CardGeneral>{/* llamar componente de  card Cris*/}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
             <CardGeneral>
-              {/* llamar componente de  card Hugo */}
+              {
+                <CardJess
+                  name="Jessica Vargas"
+                  image="/jess-vargas.jpeg"
+                  profession="Soy diseñadora UI y actualmente trabajo en una agencia llamada Convertia, me encuentro estudiando para convertirme en desarrolladora web. Me encanta dibujar para relajar mi cabeza y disfruto mucho tomar un cafecito, escuchar música y diseñar"
+                />
+              }
             </CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Hector */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Rubs */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Cris*/}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Robert */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Jess */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Erick */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Rubs */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card Heidel */}</CardGeneral>
           </div>
           <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Robert */}
-            </CardGeneral>
-          </div>
-          <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Erick */}
-            </CardGeneral>
-          </div>
-          <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card Heidel */}
-            </CardGeneral>
-          </div>
-          <div class="col-12 col-md-4 col-lg-3 mt-2">
-            <CardGeneral>
-              {/* llamar componente de  card fer */}
-            </CardGeneral>
+            <CardGeneral>{/* llamar componente de  card fer */}</CardGeneral>
           </div>
         </div>
-    </section>
+      </section>
     </>
   );
 }
